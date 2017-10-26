5e3b4177bb115a9b975d4d57f095f0493a714ec9
375956133 <375956133@qq.com>
2017-10-26 09:56:54 +0800

    yixieziliao

---
+++ 一些小的作品集/阿里云网站/js/index.js
@@ -0,0 +1,42 @@
+/**
+ * Created by Administrator on 2017/1/21.
+ */
+
+$("#ycp-puclic").mouseover(function(){
+    $(this).css("width","310px");
+    $("#ycp-public-top").css("width","310px").css("backgroundColor"," #00C1DE");
+})
+$("#ycp-puclic").mouseout(function(){
+    $(this).css("width","");
+    $("#ycp-public-top").css("width","");
+    $("#ycp-public-top").css("backgroundColor","");
+
+})
+$("#ycp-puclic-2").mouseover(function(){
+    $(this).css("width","310px");
+    $("#ycp-public-top-2").css("width","310px").css("backgroundColor"," #00C1DE");
+})
+$("#ycp-puclic-2").mouseout(function(){
+    $(this).css("width","");
+    $("#ycp-public-top-2").css("width","");
+    $("#ycp-public-top-2").css("backgroundColor","");
+
+})
+$("#ycp-puclic-3").mouseover(function(){
+    $(this).css("width","310px");
+    $("#ycp-public-top-3").css("width","310px").css("backgroundColor"," #00C1DE");
+})
+$("#ycp-puclic-3").mouseout(function(){
+    $(this).css("width","").css("backgroundColor","");
+    $("#ycp-public-top-3").css("width","").css("backgroundColor","");
+
+})
+$("#ycp-puclic-4").mouseover(function(){
+    $(this).css("width","310px");
+    $("#ycp-public-top-4").css("width","310px").css("backgroundColor"," #00C1DE");
+})
+$("#ycp-puclic-4").mouseout(function(){
+    $(this).css("width","").css("backgroundColor","");
+    $("#ycp-public-top-4").css("width","").css("backgroundColor","");
+
+})
